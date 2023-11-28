62edceb9eebd36168dc22e0c53c016861ddab227
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-28 14:50:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+// Define the student object with a 'name' property
+const student = {
+  name: "John Doe"
+};
+
+// Add a property to the Object prototype called 'getKeys'
+Object.prototype.getKeys = function() {
+  // Use Object.keys() to get an array of all keys in the object
+  return Object.keys(this);
+};
+
+// Test the getKeys() method on the 'student' object
+const keysArray = student.getKeys();
+console.log(keysArray); // Output: ['name']
